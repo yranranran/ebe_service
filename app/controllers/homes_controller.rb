3ade337b0c876dabdaddf6fea29dc62a4df0cda4
yranranran <aoyuka@gmail.com>
2022-07-01 20:27:29 +0900

    Fix: ruby version, User登録とログイン

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,5 @@
+class HomesController < ApplicationController
+  skip_before_action :require_login, only: %i[show]
+
+  def show; end
+end
--- app/controllers/supports_controller.rb
+++
@@ -1,3 +0,0 @@
-class SupportsController < ApplicationController
-  def top; end
-end
