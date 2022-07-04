f558311e1c5f21d34ff8b41c111d23a1ede8bbd0
yranranran <aoyuka@gmail.com>
2022-07-04 19:58:50 +0900

    Add: 質問新規投稿

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :user
 
-  validates :title, presence: true, length: { maximum: 255 }
-  validates :body, presence: true, length: { maximum: 65_535 }
+  validates :title, presence: true, length: { maximum: 60 }
+  validates :body, presence: true, length: { maximum: 1_000 }
 end
