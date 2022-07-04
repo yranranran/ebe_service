3977523155e28a456f3fba310a5252843241bf3b
yranranran <aoyuka@gmail.com>
2022-07-04 12:48:20 +0900

    Add: 質問一覧

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
+
+  private
+
+  def not_authenticated
+    flash[:alert] = t('defaults.message.require_login')
+    redirect_to login_path
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -43,5 +43,8 @@ module EbeService
 
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
