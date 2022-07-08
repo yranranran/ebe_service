6fd3d14ed37a5ea772504665302704e2bdfd2392
yranranran <aoyuka@gmail.com>
2022-07-08 20:24:49 +0900

    Add: bookmark question

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :questions
   has_many :answers
   has_many :evidences
+  has_many :bookmark_questions, dependent: :destroy
+  has_many :bookmark_questions_questions, through: :bookmark_questions, source: :question
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
@@ -14,5 +16,17 @@ class User < ApplicationRecord
 
   def own?(object)
     id == object.user_id
+  end
+
+  def bookmark(question)
+    bookmark_questions_questions << question
+  end
+
+  def unbookmark(question)
+    bookmark_questions_questions.destroy(question)
+  end
+
+  def bookmark?(question)
+    bookmark_questions_questions.include?(question)
   end
 end
