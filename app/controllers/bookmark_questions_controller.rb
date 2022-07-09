522ec204a1c07c975775888768d29ad3de350ab7
yranranran <aoyuka@gmail.com>
2022-07-09 20:25:00 +0900

    Add: bookmark question ajax

--- app/controllers/bookmark_questions_controller.rb
+++ app/controllers/bookmark_questions_controller.rb
@@ -1,13 +1,11 @@
 class BookmarkQuestionsController < ApplicationController
   def create
-    question = Question.find(params[:question_id])
-    current_user.bookmark(question)
-    redirect_back fallback_location: root_path, alert: t('.success')
+    @question = Question.find(params[:question_id])
+    current_user.bookmark(@question)
   end
 
   def destroy
-    question = current_user.bookmark_questions.find(params[:id]).question
-    current_user.unbookmark(question)
-    redirect_back fallback_location: root_path, alert: t('.success')
+    @question = current_user.bookmark_questions.find(params[:id]).question
+    current_user.unbookmark(@question)
   end
 end
