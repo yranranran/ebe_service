ca74597df54f7c5e6c0780958cfe31e7fc49f6f3
yranranran <aoyuka@gmail.com>
2022-07-11 20:04:58 +0900

    fix: like answers to ajax

--- app/controllers/like_answers_controller.rb
+++ app/controllers/like_answers_controller.rb
@@ -1,13 +1,11 @@
 class LikeAnswersController < ApplicationController
   def create
-    answer = Answer.find(params[:answer_id])
-    current_user.like_answer(answer)
-    redirect_back fallback_location: root_path, success: t('.success')
+    @answer = Answer.find(params[:answer_id])
+    current_user.like_answer(@answer)
   end
 
   def destroy
-    answer = current_user.like_answers.find(params[:id]).answer
-    current_user.unlike_answer(answer)
-    redirect_back fallback_location: root_path, success: t('.success')
+    @answer = current_user.like_answers.find(params[:id]).answer
+    current_user.unlike_answer(@answer)
   end
 end
