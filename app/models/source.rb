dbcbace06a5b7e69d9fb7f5388c25624c0a0363f
yranranran <aoyuka@gmail.com>
2022-07-14 11:06:24 +0900

    Add: dashboard

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,11 +1,11 @@
 class Source < ApplicationRecord
-  has_many :evidences, through: :evidence_sources
   has_many :evidence_sources
-  has_many :authors, through: source_authors
+  has_many :evidences, through: :evidence_sources
   has_many :source_authors
+  has_many :authors, through: :source_authors
   
   enum evidence_level:{ ★☆☆☆: 1, ★★☆☆: 2, ★★★☆: 3, ★★★★: 4 }
-  enum source_type:{ 論文:0, 書籍:1 }
+  enum source_type:{ 論文: 0, 書籍: 1 }
 
   validates :title, presence: true, length: { maximum: 60 }
   validates :body, presence: true, length: { maximum: 65_000 }
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < Taxonomy
   has_many :article_tags
-  has_many :articles, through: :article_tags, as: :articable
+  has_many :evidences, through: :article_tags, source: :articable, source_type: "Evidence"
+  has_many :questions, through: :article_tags, source: :articable, source_type: "Question"
 end
